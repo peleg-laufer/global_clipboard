2da57eb4ebc970ed572fe0ff5abf64e64ac52d29
peleg <lauferpeleg@gmail.com>
2026-03-14 15:23:37 +0200

    first commit

---
+++ clip_api.py
@@ -0,0 +1,160 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Request
+from typing import List, Optional
+from enum import IntEnum
+from pydantic import BaseModel,Field
+import clip_db_handler
+from clip_db_handler import FileMeta, PublicFileMeta
+from fastapi.responses import FileResponse
+
+ALLOWED_SLOTS = {0,1,2}
+PRE_EXISTING_FILES_SLOT = -1
+api = FastAPI()
+
+@api.on_event("startup")
+async def initialize_system():
+    """
+    Triggers the database setup automatically when FastAPI boots.
+    """
+    await clip_db_handler.setup_db()
+
+@api.get("/files/{uuid}")
+async def get_file_meta(uuid: str) -> FileMeta:
+    """get metadata of file from db
+
+    Args:
+        uuid (str): uuid of file
+
+    Raises:
+        HTTPException: 404 not found if file not found
+
+    Returns:
+        FileMeta: metadata of file
+    """
+    to_ret = await clip_db_handler.get_file_meta(uuid)
+    if to_ret:
+        return to_ret
+    else:
+        raise HTTPException(status_code=404, detail="file not found")
+    
+@api.get("/files/{uuid}/download")
+async def get_file_data(uuid: str) -> FileResponse:
+    """get a file from db
+
+    Args:
+        uuid (str): uuid of file
+
+    Raises:
+        HTTPException: 404 file not found if no file on server with this uuid
+
+    Returns:
+        FileResponse: the wanted file
+    """
+    file_meta = await clip_db_handler.get_file_meta(uuid)
+    if file_meta:
+        file_response = FileResponse(path=file_meta.file_path,
+                                    filename=file_meta.file_name,
+                                    media_type=file_meta.file_type)
+        return file_response
+    else:
+        # file_meta is None == file not found
+        raise HTTPException(status_code=404, detail="file not found")
+        
+                
+@api.get("/files")
+async def get_all_files_meta(first_n: int = 0) -> List[FileMeta]:
+    """returns all the metadata of files from a given index
+
+    Args:
+        first_n (int, optional): index to start from. Defaults to 0.
+
+    Returns:
+        List[FileMeta]: a list of the metadata of all files
+    """
+    return await clip_db_handler.get_all_files_meta()
+
+async def add_file_to_taken_slot(uploaded_file: UploadFile, file_in_slot: FileMeta) -> FileMeta:
+    """makes sure user wants to replace
+    replace
+
+    Args:
+        uploaded_file (UploadFile): file to upload
+        file_in_slot (FileMeta): existing file
+
+    Raises:
+        HTTPException: 
+
+    Returns:
+        _type_: _description_
+    """
+    # TODO: ask user if wants to replace
+    return await replace_file(uuid=file_in_slot.file_uuid,
+                        new_file=upload_file)
+
+
+@api.post("/files")
+async def upload_file(uploaded_file: UploadFile, slot: int) -> FileMeta:
+    """upload a file to server
+
+    Args:
+        uploaded_file (UploadFile): the file needs to be upload
+
+    Returns:
+        FileMeta: the metadata of the stored file. None if no success
+    """
+    print("got file: " + str(uploaded_file))
+    print(f"to enter to slot: {slot}")
+    if slot not in ALLOWED_SLOTS:
+        raise HTTPException(status_code=400, detail=f"slot {slot} not in allowed slots: {ALLOWED_SLOTS}")
+    file_in_slot = await clip_db_handler.get_file_meta_in_slot(slot)
+    print(f"file in slot to replace: {file_in_slot}")
+    if file_in_slot:
+        return await add_file_to_taken_slot(uploaded_file=uploaded_file,
+                                      file_in_slot=file_in_slot)
+    else:
+        uploaded_file_meta = await clip_db_handler.add_file(uploaded_file, slot)
+        if uploaded_file_meta:
+            return uploaded_file_meta
+        else:
+            raise HTTPException(status_code=500, detail="unable to upload file")
+    
+
+@api.put("/files/{uuid}/replace")
+async def replace_file(uuid: str, new_file: UploadFile) -> FileMeta:
+    """
+    replaces a file in the server
+    Args:
+        file_id (int): id of the file that needs to be replaced
+        new_file (FileBase): the file to put in
+    
+    Raises:
+        HTTPException: no file matching uuid given
+
+    Returns:
+        File: the file that was added, including id
+    """
+    added_file = await clip_db_handler.replace_file(uuid,new_file)
+    if not added_file:
+        print("file was not replaced")
+        raise HTTPException(status_code=404, detail="file not found")
+    else:
+        return added_file
+    
+        
+@api.delete("/files/{uuid}")
+async def remove_file(uuid: str) -> FileMeta:
+    """deletes a file based on uuid
+
+    Args:
+        uuuid (str): uuid of file to delete
+
+    Raises:
+        HTTPException: no file matching uuid given
+
+    Returns:
+        FileMeta: metadata of file that was removed
+    """    
+    removed = await clip_db_handler.remove_file(uuid)
+    if removed:
+        return removed
+    else:
+        raise HTTPException(status_code=404, detail="file not found")
---
+++ clip_db_handler.py
@@ -0,0 +1,293 @@
+import os
+from typing import List, Optional, Literal
+from enum import IntEnum
+from pydantic import BaseModel,Field
+from fastapi import UploadFile
+import uuid
+from pymongo import AsyncMongoClient
+import asyncio
+import mimetypes
+
+print("RUNNING FROM THIS EXACT FILE:")
+print(os.path.abspath(__file__))
+
+class PublicFileMeta(BaseModel):
+    """
+    safe info to send to client
+    """
+    file_name: str = Field(..., min_length=1, max_length=512, description="file name")
+    file_type: str = Field(..., description="file type")
+    file_size: int = Field(..., description="file size in bytes")
+    
+
+class FileMeta(PublicFileMeta):
+    """
+    PublicFileMeta with internal server info
+    """
+    file_slot: int = Field(..., description="serial number of file")
+    file_path: str = Field(..., min_length=1, max_length=512, description="file path")
+    file_uuid: str = Field(..., min_length=1, max_length=512, description="file unique id")
+
+class IllegalSlotError(Exception):
+    """
+    raised when illegal slot given from client
+    """
+    pass
+
+FILES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "files")
+ALLOWED_SLOTS = {0,1,2}
+PRE_EXISTING_FILES_SLOT = -1
+CONNECTION_STRING = "mongodb://localhost:27017/"
+client = AsyncMongoClient(CONNECTION_STRING)
+db = client["clipboard_db"]
+files_collection = db["files_meta"]
+
+
+async def setup_db():
+    # TODO: break into readable stuff
+    """
+    Verifies connection, creates folder, sets up indexes.
+    fixes mismatch between folder and db: if on db and not folder - delete
+                                        if on folder and not on db - add to db
+    """
+    print(f"setting up db")
+    # create file dir
+    if not os.path.exists(FILES_PATH):
+        os.makedirs(FILES_PATH)
+
+    # check db
+    await client.admin.command("ping")
+    print(f"    MongoDB connected")
+
+    # making uuid unique index
+    await files_collection.create_index("file_uuid", unique=True)
+    print(f"    Index created")
+
+    # verify db and files in folder match, ignoring sub-folders
+    files_in_folder = []
+    for poss_file in os.listdir(FILES_PATH):
+        poss_file_path = os.path.join(FILES_PATH, poss_file)
+        if os.path.isfile(poss_file_path): # ignoring folders
+            files_in_folder.append(poss_file)
+    print(f"    files on folder:")
+    for filename in files_in_folder:
+        print(f"        {filename}")
+    files_on_db = await get_all_files_meta()
+    print(f"    files on db: ")
+    for file_meta in files_on_db:
+        print(f"        ", file_meta.file_name)
+    print(f"    deleting wrong metas in db")
+    # verify db and deleting wrong metas:
+    for file_meta in files_on_db:
+        if not os.path.exists(file_meta.file_path):
+            print(f"        deleting ", file_meta.file_name, " from db")
+            await remove_file(file_meta.file_uuid)  # TODO: not removing, debug
+
+    # TODO: verify legal slots, no duplicates of slots
+
+    # verify file directory and adding missing files to db:
+    print("     adding missing files to db")
+    for file_name in files_in_folder:
+        print("         checking ", file_name)
+        file_path = os.path.join(FILES_PATH, file_name)
+        print("             path: ", file_path)
+        matching_files_in_db = await files_collection.find({"file_path": file_path}).to_list(None)
+        print("             db documents matching path: ")
+        print("             ", matching_files_in_db)
+        if len(matching_files_in_db) == 0:
+            # finding mime type of file
+            print("             no db documents matching file")
+            file_mime_type = mimetypes.guess_type(file_name)[0]
+            new_file_uuid = str(uuid.uuid4())
+            # renaming file to uuid
+            ext = os.path.splitext(file_name)[1]
+            new_file_path = os.path.join(FILES_PATH, new_file_uuid + '.' + ext)
+            os.rename(file_path, new_file_path)
+            # constracting missing FileMeta
+            missing_file_meta = FileMeta(file_name=file_name,
+                                         file_type=str(file_mime_type),
+                                         file_size=os.path.getsize(new_file_path),
+                                         file_slot=PRE_EXISTING_FILES_SLOT, # not it any slot
+                                         file_path=new_file_path,
+                                         file_uuid=new_file_uuid)
+            inserted_meta = await files_collection.insert_one(missing_file_meta.model_dump())
+            print("             added ", inserted_meta, " to db")
+        elif len(matching_files_in_db) >= 2:
+            # TODO: delete duplicates
+            pass
+            
+
+async def get_file_meta(uuid: str) -> FileMeta:
+    # TODO: add slot choosing
+    """gets a specific file by uuid
+
+    Args:
+        uuid (str): uuid of file
+
+    Returns:
+        FileMeta: metadata of requested file
+    """
+    print(f"finding meta of uuid: {uuid}")
+    doc = await files_collection.find_one({"file_uuid": uuid}, {"id_": 0})
+    if doc:
+        print(f"    found file: {FileMeta(**doc).file_name}")
+        return FileMeta(**doc)
+    else:
+        print(f"    no document matching uuid")
+        return None
+
+async def get_file_meta_in_slot(slot: int) -> FileMeta:
+    """returns the meta of file in given slot
+
+    Args:
+        slot (int): slot of file. legal values: [0,1,2]
+
+    Returns:
+        FileMeta: _description_
+    """
+    print(f"finding FileMeta in slot: {slot}")
+    if slot not in ALLOWED_SLOTS:
+        raise IllegalSlotError(f"slot {slot} given is illegal value, not in allowed slots: {ALLOWED_SLOTS}")
+    doc = await files_collection.find_one({"file_slot": slot}, {"id_": 0})
+    if doc:
+        print(f"    found file in slot: {FileMeta(**doc).file_name}")
+        return FileMeta(**doc)
+    else:
+        print(f"    no file found")
+        return None
+
+async def get_file_path(uuid: str) -> str:
+    """returns path of file
+
+    Args:
+        uuid (str): uuid of file
+
+    Returns:
+        str: path of file
+    """
+    print(f"finding path of uuid: {uuid}")
+    file_meta = get_file_meta(uuid)
+    if file_meta:
+        print(f"    found path: {file_meta.file_path}")
+        return file_meta.file_path
+    else:
+        print(f"    no path found")
+        return None
+        
+
+async def get_all_files_meta(first_n: int = 0) -> List[FileMeta]:
+    """returns all the files from a given index, if not given returns all
+
+    Args:
+        first_n (int, optional): index to start from. Defaults to 0.
+
+    Returns:
+        List[File]: a list of the files
+    """
+    print(f"getting all files meta from {first_n}")
+    cursor = files_collection.find({},
+                                   {"_id": 0})
+    files_dict = await cursor.to_list()
+    files_filemeta = []
+    for file_dict in files_dict:
+        files_filemeta.append(FileMeta(**file_dict))
+    for filemeta in files_filemeta[first_n:]:
+        print(f"    {filemeta}")
+    return files_filemeta[first_n:]
+
+
+
+async def add_file(uploaded_file: UploadFile, slot: int)  -> FileMeta:
+    # TODO: add 3 slot logic. make slot 0-2 unique somehow. maybe change the non slot to none!
+    """add a given file to the server
+
+    Args:
+        uploaded_file (UploadFile): the file to add 
+        slot (int) [0,1,2]: the slot for file
+
+    Returns:
+        File: the file that was added, including id
+    """
+    print(f"adding file to slot: {slot}")
+    print(f"file: {uploaded_file}")
+    if slot not in ALLOWED_SLOTS:
+        raise IllegalSlotError(f"slot {slot} given is illegal value, not in {ALLOWED_SLOTS}")
+    file_in_slot = await get_file_meta_in_slot(slot)        
+    if isinstance(file_in_slot, FileMeta):
+        print(f"    file in slot {slot}: {file_in_slot.file_name}")
+        raise IllegalSlotError(f"slot {slot} is taken by {file_in_slot.file_name}")
+    else:
+        print(f"    no file in slot")
+    # setting up metadata:
+    new_file_uuid = str(uuid.uuid4())
+    ext = os.path.splitext(uploaded_file.filename)[1]
+    new_file_path = os.path.join(FILES_PATH, new_file_uuid + ext)
+    new_file_name = uploaded_file.filename
+    new_file_size = uploaded_file.size
+    new_file_type = uploaded_file.content_type
+    new_file_metadata = FileMeta(file_name=new_file_name,
+                                 file_type=new_file_type,
+                                 file_size=new_file_size,
+                                 file_slot=slot,
+                                 file_path=new_file_path,
+                                 file_uuid=new_file_uuid)
+    print(f"    new file metadata: ", str(new_file_metadata))
+    # writing into local file:
+    try:
+        print(f"    reading file content")
+        file_content = uploaded_file.file.read()
+        print(f"    writing new file on server in path: {new_file_metadata.file_path}")
+        with open(new_file_path, "wb") as new_file:
+            new_file.write(file_content)
+        inserted_file = await files_collection.insert_one(new_file_metadata.model_dump())
+        return new_file_metadata
+    except Exception as e:
+        print("error: ", e)
+
+
+async def replace_file(uuid: str, new_file: UploadFile) -> FileMeta:
+    """replace a file on server
+
+    Args:
+        uuid (str): uuid of the soon to be replaced file
+        new_file (UploadFile): file to upload intead
+
+    Returns:
+        FileMeta: metadata of new file on server. None if no file with uuid given
+    """
+    print("replacing uuid: ", uuid, " and putting: ", new_file)
+    removed = await remove_file(uuid)
+    print(f"    removed: ", removed)
+    if removed:
+        added_file = await add_file(uploaded_file=new_file, slot=int(removed['file_slot']))
+        print(f"    added: ", added_file)
+        return added_file
+    else:
+        return None
+    
+        
+async def remove_file(uuid: str) -> FileMeta:
+    # TODO: not working, debug
+    """removes a file based on uuid
+
+    Args:
+        uuid (str): uuid of file to delete
+
+    Raises:
+        HTTPException: no file matching uuid given
+
+    Returns:
+        FileMeta: meta of file that was removed
+    """
+    print(f"removing uuid: {uuid}")
+    to_remove = await files_collection.find_one_and_delete({"file_uuid": uuid}, {"_id": 0})
+    if to_remove:
+        path_to_remove = to_remove["file_path"]
+        print(f"    trying to remove path: ", path_to_remove)
+        if os.path.exists(path_to_remove):
+            os.remove(path_to_remove)
+        else:
+            print(f"    no file matching path saved in db: ", to_remove)
+        return to_remove
+    else:
+        return None
